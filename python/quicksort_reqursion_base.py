5aa61a8cf3513373f8c58503707c14f2e39f3424
Artyom Pak <44568508+artyomvp1@users.noreply.github.com>
2022-05-16 23:22:59 -0700

    Update quicksort_reqursion_base.py

--- python/quicksort_reqursion_base.py
+++ python/quicksort_reqursion_base.py
@@ -1,38 +1,15 @@
-# Option 2
-def quick1(sequence):
-    if len(sequence) <= 1:
-        return sequence
-    else:
-        pivot_point = sequence[len(sequence) // 2]    # pivot point in the middle
-    
-    less = []
-    more = []
-    
-    for i in sequence:
-        if i > pivot_point:
-            more.append(i)
-        elif i == pivot_point:    # iteration should not go over pivot point
-            continue
-        else:
-            less.append(i)
-    
-    return quick1(less) + [pivot_point] + quick1(more)
-
-
-print(quick1([7, 5, 6, 3, 4, 1, 2]))
-
-
-# The most delicate
 def quick(sequence):
+    
     if len(sequence) <= 1:
         return sequence
     else:
-        pivot = sequence[len(sequence) // 2]
+        pivot_point = sequence[len(sequence) // 2]
+        sequence.remove(pivot_point)
     
-    more = quick([i for i in sequence if i > pivot if i != pivot])
-    less = quick([j for j in sequence if j <= pivot if j != pivot])
+    less = quick([i for i in sequence if i < pivot_point])
+    more = quick([j for j in sequence if j >= pivot_point])
     
-    return less + [pivot] + more
+    return less + [pivot_point] + more
+    
 
-
-print(quick([5, 3, 4, 2, 1]))
+print(quick([4, 3, 2, 2, 1, 6, 5, 5, 5]))
