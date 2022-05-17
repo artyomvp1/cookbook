150feb54eea27f4e448265c21136da17aed27299
Artyom Pak <44568508+artyomvp1@users.noreply.github.com>
2022-05-16 21:42:56 -0700

    Update quicksort_reqursion_base.py

--- python/quicksort_reqursion_base.py
+++ python/quicksort_reqursion_base.py
@@ -1,8 +1,38 @@
-# Should be optimized
-def qsort(list):
-    if list == []: 
-        return []
-    pivot = list[0]
-    l = qsort([x for x in list[1:] if x < pivot])
-    u = qsort([x for x in list[1:] if x >= pivot])
-    return l + [pivot] + u
+# Option 2
+def quick1(sequence):
+    if len(sequence) <= 1:
+        return sequence
+    else:
+        pivot_point = sequence[len(sequence) // 2]    # pivot point in the middle
+    
+    less = []
+    more = []
+    
+    for i in sequence:
+        if i > pivot_point:
+            more.append(i)
+        elif i == pivot_point:    # iteration should not go over pivot point
+            continue
+        else:
+            less.append(i)
+    
+    return quick1(less) + [pivot_point] + quick1(more)
+
+
+print(quick1([7, 5, 6, 3, 4, 1, 2]))
+
+
+# The most delicate
+def quick(sequence):
+    if len(sequence) <= 1:
+        return sequence
+    else:
+        pivot = sequence[len(sequence) // 2]
+    
+    more = quick([i for i in sequence if i > pivot if i != pivot])
+    less = quick([j for j in sequence if j <= pivot if j != pivot])
+    
+    return less + [pivot] + more
+
+
+print(quick([5, 3, 4, 2, 1]))
