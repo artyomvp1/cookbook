44c3f3f32b9c1ac707ecd97740c6e41c85dfd306
Artyom Pak <44568508+artyomvp1@users.noreply.github.com>
2022-09-17 01:10:09 -0700

    Template quick sort

---
+++ python/sort_algo_quick.py
@@ -0,0 +1,13 @@
+def quick(sequence):
+    
+    if len(sequence) <= 1:
+        return sequence
+    
+    pivot_point = sequence[0]
+    
+    less = [i for i in sequence if i < pivot_point]
+    more = [j for j in sequence if j > pivot_point]
+    midd = [k for k in sequence if k == pivot_point]
+    
+    return quick(less) + midd + quick(more
+                                      
