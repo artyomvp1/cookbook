893f11fe7dfef9d1d90946a84c5a1d0782288ac4
Artyom Pak <44568508+artyomvp1@users.noreply.github.com>
2023-10-17 14:44:03 -0700

    Tutorial 6

---
+++ selenium/tutorial-6-multy-elements.py
@@ -0,0 +1,18 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+driver.get('https://www.yatra.com')
+driver.maximize_window()
+time.sleep(2)
+
+sequence = driver.find_elements(By.TAG_NAME, 'a')  # creates a list of tags with 'a'
+print(len(sequence))  # prints it
+
+for i in sequence:
+    print(i.text)  # print the content of the tag
+
+
+driver.close()
