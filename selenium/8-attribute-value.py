4b890ce7a9ab442c1eb1003397a4d1518b62f375
Artyom Pak <44568508+artyomvp1@users.noreply.github.com>
2023-11-02 20:43:11 -0700

    Tutorial

--- selenium/8-attribute-value.py
+++ selenium/8-attribute-value.py
@@ -2,14 +2,13 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
+""" 
+Webelements may contain attributes, data-role, type, value, id, class, etc.
+<input data-role="none" type="submit" value="Search Flights" id="BE_flight_flsearch_btn" class="js_ripple search-btn " xpath="1"> - this is a web element.
+"""
 driver = webdriver.Chrome()
 driver.get('https://www.yatra.com')
 time.sleep(2)
-
-""" The webelement contains attributes, data-role, type, value, id, class, etc.
-<input data-role="none" type="submit" value="Search Flights" id="BE_flight_flsearch_btn" class="js_ripple search-btn " xpath="1"> - this is a web element. It has attrubutes and their values
-"""
 
 attr = driver.find_element(By.XPATH, "//div[@class='ripple-parent search-height demo-icon icon-go']//input[@id='BE_flight_flsearch_btn']").get_attribute("value")  # get_attribute() - method to get
 
