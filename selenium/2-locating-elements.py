ac1a77f5b3abeb20fa1eae730e309f70ac905bf4
Artyom Pak <44568508+artyomvp1@users.noreply.github.com>
2023-11-02 20:36:05 -0700

    Tutorial

--- selenium/2-locating-elements.py
+++ selenium/2-locating-elements.py
@@ -1,14 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By  # Library to locate "by"
 
-
+"""
+Find web element locators using 'inspect' functionality.
+You can inspect by mouse.
+"""
 driver = webdriver.Chrome()
 driver.get('https://secure.yatra.com/social/common/yatra/signin.htm')
 driver.maximize_window()
-driver.find_element(By.ID, "login-input").send_keys('Test')  # special library for By
+
+# Locating an element
+driver.find_element(By.ID, "login-input").send_keys('Test')  # Sending text "test" to the field
 
 driver.close()
