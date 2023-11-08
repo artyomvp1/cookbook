9dc3c6772318155d6ae954b04583047d89952a96
Artyom Pak <44568508+artyomvp1@users.noreply.github.com>
2023-11-07 16:26:58 -0800

    tutorial

---
+++ pytest/2-command-line.py
@@ -0,0 +1,10 @@
+"""
+Using terminal go to the folder with all the test-files.
+Command: pytest
+python -v (verbose) shows the result with more details.
+-s shoes print statements
+"""
+
+
+def testSecond():
+    assert 1+1 == 2
