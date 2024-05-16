6d90d6a3b826334d7eea1aecbbac8c572c218225
Artyom Pak <44568508+artyomvp1@users.noreply.github.com>
2024-05-15 18:39:50 -0700

    1

--- tkinter/1-create-window.py
+++ tkinter/1-create-window.py
@@ -2,4 +2,13 @@ import tkinter as tk
 
 
 root = tk.Tk()
+
+root.geometry("800x500")    # Resolution
+root.title("My First GUI")    # Title
+
+label = tk.Label(root,
+                 text="Hello",
+                 font=("Arial", 22))
+label.pack(padx=20, pady=30)  # Location of the label?
+
 root.mainloop()
